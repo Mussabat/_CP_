201f20fef8943cb6b0c622f6a5d40dd494b6333c
Nafisa Tabassum <tnafisa18@gmail.com>
2022-02-14 17:40:40 +0600

    Add files via upload

---
+++ OOPs/oop_object.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class student {
+    public :
+    string name;  /// attributes dilam
+    int age;
+    bool gender;
+
+};
+
+
+int main() {
+
+    student a;
+    a.name = "Nafisa";
+    a.age = 21;
+    a.gender = 1;
+
+    return 0;
+}
