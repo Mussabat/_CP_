7b1943d042074b1433370c040c667a4b5bbd3654
Nafisa Tabassum <tnafisa18@gmail.com>
2022-08-28 21:54:57 +0600

    Add files via upload

---
+++ Array/Linked_List_Traverse.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+struct node {
+    ll val;
+    node* next;
+};
+
+void traverse(node* head) {
+    node* cur = head;
+    while(cur->next != NULL) {
+        cur = cur->next;
+        cout << cur->val << '\n';
+    }
+}
+
+int main() {
+    node* head = new node;
+    head->next = NULL;
+    node* last;
+    last = head;
+    ll n;
+    cin >> n;
+    for(ll i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        node* cur = new node;
+        cur->val = x;
+        cur->next = NULL;
+        last->next = cur;
+        last = cur;
+    }
+
+    traverse(head);
+
+
+}
+
